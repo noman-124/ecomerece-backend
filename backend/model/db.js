281b1e2b9fe2978b039d81ddca9496497b9e4b64
noman-124 <muhammadnoman4474@gmail.com>
2024-09-23 16:53:27 +0500

    ajdfl;

--- backend/model/db.js
+++ backend/model/db.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 
 try {
-    let connect = mongoose.connect(process.env.MONGODB_URI);
+    let connect = mongoose.connect("mongodb+srv://sharjeelakhtar245:sharjeel3322@cluster0.ssd7alz.mongodb.net/");
     console.log("db connected")
 } catch (e) {
     console.log(e)
